125da595ea1d170211176d10c9f9c54c14db47ba
jeremiah <jboothwebdev@gmail.com>
2021-07-16 12:40:33 -0700

    add a product details component

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar"
 import DisplayProducts from "./components/DisplayProducts/displayProducts";
+import { ProductDetails } from "./components/ProductDetails/ProductDetails";
 
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [productID, setProductId] = useState();
 
   useEffect(async()=>{
     let response = await axios.get(`https://localhost:44394/api/products`);
@@ -23,16 +25,13 @@ function App() {
   }
   return (
     <div className="App">
-<<<<<<< HEAD
-      <NavBar searchProducts={searchProducts}/>
-=======
      <NavBar searchProducts={searchProducts}/>
-     <DisplayProducts products={products}/>
->>>>>>> 05aa1f9fc43c57ceeab0512fef9176497ebe30fa
+     <DisplayProducts products={products} productID={productID} setProductId={setProductId}/>
 
       {/* links to other pages inside of switch    */}
      <Switch>
       <Route path="/register" component={RegistrationForm}/>
+      <Route path={"/product/" + productID} render={props => <ProductDetails {...props} productID={productID}/>}/>
       <Redirect to="/not-found"/>
      </Switch>
     </div>
---
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ProductDetails = (props) => {
+    return (
+        <div>
+            <h1>{props.productID}</h1>
+        </div>
+    )
+}
