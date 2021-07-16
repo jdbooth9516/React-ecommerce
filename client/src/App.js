05aa1f9fc43c57ceeab0512fef9176497ebe30fa
unknown <mrchrisfarrell@yahoo.com>
2021-07-16 12:46:02 -0400

    display products works

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,16 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar"
+import DisplayProducts from "./components/DisplayProducts/displayProducts";
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  useEffect(async()=>{
+    let response = await axios.get(`https://localhost:44394/api/products`);
+    console.log(response.data);
+    setProducts(response.data);
+  },[]);
 
   const searchProducts = async (searchInput) => {
     let response = await axios.post(`https://localhost:44394/api/products/name`, searchInput);
@@ -16,6 +23,7 @@ function App() {
   return (
     <div className="App">
      <NavBar searchProducts={searchProducts}/>
+     <DisplayProducts products={products}/>
 
       {/* links to other pages inside of switch    */}
      <Switch>
